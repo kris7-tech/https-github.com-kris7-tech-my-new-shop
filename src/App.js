90c45ed661894ba6760281c3d0b15820418a18d8
kris7-tech <73724252+kris7-tech@users.noreply.github.com>
2022-10-28 18:20:32 +0200

    added typescript

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import { useEffect } from 'react';
+import { useEffect, lazy, Suspense } from 'react';
 import { useDispatch } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
-import Home from './routes/home/home.component';
-import Navigation from './routes/navigation/navigation.component';
-import Authentication from './routes/authentication/authentication.component';
-import Shop from './routes/shop/shop.component';
-import CheckOut from './routes/checkout/checkout.component';
+import Spinner from './components/spinner/spinner.component';
 import { checkUserSession } from './store/user/user.action';
+import { GlobalStyle } from './global.styles'; 
 
+
+const Home = lazy(() =>import('./routes/home/home.component'));
+const Authentication = lazy(() => import('./routes/authentication/authentication.component'));
+const Shop = lazy(() =>import('./routes/shop/shop.component'));
+const CheckOut = lazy(() => import('./routes/checkout/checkout.component'));
+const Navigation = lazy(() => import('./routes/navigation/navigation.component'));
 
 const App = () => {
   const dispatch = useDispatch();
@@ -17,6 +20,8 @@ const App = () => {
   }, []);
   
   return (
+    <Suspense fallback={<Spinner/>}>;
+    <GlobalStyle/>
     <Routes>
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
@@ -25,6 +30,7 @@ const App = () => {
         <Route path='checkout' element={<CheckOut />} />
       </Route>
     </Routes>
+  </Suspense>
   );
 };
 
